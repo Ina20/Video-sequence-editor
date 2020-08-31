ca6b3131c28665fd5939f9524290d1acea4c3998
Ina20 <inaszkola@gmail.com>
2020-08-31 15:29:36 +0200

    Open Editor

--- scripts/uploadVideo.js
+++ scripts/uploadVideo.js
@@ -24,11 +24,11 @@ document.querySelectorAll(".drop-zone-input").forEach(inputElement => {
 
   dropZoneElement.addEventListener("drop", e => {
     e.preventDefault();
-    console.log(e.dataTransfer.files);
+    //console.log(e.dataTransfer.files);
 
     if(e.dataTransfer.files.length) {
       inputElement.files = e.dataTransfer.files;
-      console.log(inputElement.files);
+      //console.log(inputElement.files);
       updateThumbnails(dropZoneElement, e.dataTransfer.files[0]);
     }
 
@@ -64,4 +64,35 @@ function updateThumbnails(dropZoneElement, file) {
   }else {
     thumbnailElement.style.backgroundImage = null;
   }
+}
+function openEditor(){
+    //document.getElementById("uploadForm").submit();
+    let file = document.getElementById("startUpload").files[0];
+
+    document.body.style.backgroundImage = "linear-gradient(12deg, rgba(193, 193, 193,0.05) 0%, rgba(193, 193, 193,0.05) 2%,rgba(129, 129, 129,0.05) 2%, rgba(129, 129, 129,0.05) 27%,rgba(185, 185, 185,0.05) 27%, rgba(185, 185, 185,0.05) 66%,rgba(83, 83, 83,0.05) 66%, rgba(83, 83, 83,0.05) 100%),linear-gradient(321deg, rgba(240, 240, 240,0.05) 0%, rgba(240, 240, 240,0.05) 13%,rgba(231, 231, 231,0.05) 13%, rgba(231, 231, 231,0.05) 34%,rgba(139, 139, 139,0.05) 34%, rgba(139, 139, 139,0.05) 71%,rgba(112, 112, 112,0.05) 71%, rgba(112, 112, 112,0.05) 100%),linear-gradient(236deg, rgba(189, 189, 189,0.05) 0%, rgba(189, 189, 189,0.05) 47%,rgba(138, 138, 138,0.05) 47%, rgba(138, 138, 138,0.05) 58%,rgba(108, 108, 108,0.05) 58%, rgba(108, 108, 108,0.05) 85%,rgba(143, 143, 143,0.05) 85%, rgba(143, 143, 143,0.05) 100%),linear-gradient(96deg, rgba(53, 53, 53,0.05) 0%, rgba(53, 53, 53,0.05) 53%,rgba(44, 44, 44,0.05) 53%, rgba(44, 44, 44,0.05) 82%,rgba(77, 77, 77,0.05) 82%, rgba(77, 77, 77,0.05) 98%,rgba(8, 8, 8,0.05) 98%, rgba(8, 8, 8,0.05) 100%),linear-gradient(334deg, rgb(237,235,215),rgb(237,235,215))";
+
+    console.log("Hello there!");
+    document.getElementById("home").style.display = "none";
+    document.getElementById("editor").style.display = "block";
+    console.log(file);
+    let fileDisplay = document.createElement('div');
+    fileDisplay.classList.add("fileListDisplay");
+    document.getElementById("videoBar").appendChild(fileDisplay);
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = () => {
+      fileDisplay.style.backgroundImage = `url('${ reader.result }')`;
+    };
+}
+function addMoreVideo(){
+    let file = document.getElementById("nextUpload").files[0];
+    console.log(file);
+    let fileDisplay = document.createElement('div');
+    fileDisplay.classList.add("fileListDisplay");
+    document.getElementById("videoBar").appendChild(fileDisplay);
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = () => {
+      fileDisplay.style.backgroundImage = `url('${ reader.result }')`;
+    };
 }
